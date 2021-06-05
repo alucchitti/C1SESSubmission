dd12da116c6f07ed589950d8270ef66031e39849
Anthony Lucchitti <ael74@pitt.edu>
2021-06-04 23:14:07 -0400

    Update styling and fetch

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const titleForm = document.querySelector('#titleForm');
 const titleInput = document.querySelector("#movieTitle");
 const msg = document.querySelector(".msg");
+const resultsDiv = document.querySelector(".results");
 
 
 // Listen for form submit
@@ -26,8 +27,27 @@ function onSubmit(e) {
 
 function displayMovies(titleInput) {
 	// Need to handle spaces
-	const url = `http://www.omdbapi.com/?t=${titleInput}&apikey=3d206f64`;
+	titleInput = titleInput.replace(/ /g, '%20')
+	const url = `http://www.omdbapi.com/?s=${titleInput}&apikey=3d206f64`;
 	fetch(url)
 		.then(response => response.json())
-		.then(data => console.log(data));
+		.then(data => outputData(data));
+}
+
+function outputData(data) {
+	// Check if there were no responses found
+	if(data.Response == "False"){
+		resultsDiv.classList.add('error');
+		resultsDiv.innerHTML = "Sorry! No results found.";
+	} else {
+		console.log(data)
+		// Remove error message if present
+		resultsDiv.remove()
+		//Display movies
+		data = data.Search;
+	    for(i = 0; i < data.length; i++) {
+	    	console.log(`Title: ${data[i].Title} Release date: ${data[i].Year} Runtime: ${data[i].Runtime} Genre: ${data[i].Genre} Director: ${data[i].Director}`);
+	    }
+	}
+		
 }
